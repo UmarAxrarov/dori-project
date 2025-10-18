603f5cfdd1be4f77fa439cc4244ffa4bd08c1588
Umar <axrarovumar6@gmail.com>
2025-10-18 17:20:52 +0500

    fetching

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,23 +2,23 @@ import { NextIntlClientProvider } from "next-intl";
 import { notFound } from "next/navigation";
 import { MainLayout } from "@/layout";
 import "../../styles/globals.css";
-export default async function LocaleLayout({
-  children,
-  params,
-}: {
-  children: React.ReactNode;
+import { ReactNode } from "react";
+
+interface LocaleLayoutProps {
+  children: ReactNode;
   params: { locale: string };
-}) {
+}
+
+export default async function LocaleLayout({ children, params }: LocaleLayoutProps) {
   const { locale } = params;
 
   let messages;
   try {
     messages = (await import(`../../locales/${locale}.json`)).default;
-    
   } catch {
     notFound();
   }
-  
+
   return (
     <html lang={locale}>
       <body>
