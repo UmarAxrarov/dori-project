8126e0ebb3010da0ceee80416217c25bd9e2a512
Umar <axrarovumar6@gmail.com>
2025-10-18 17:46:47 +0500

    fetching_duble_trhee

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,17 +1,20 @@
-// src/app/[locale]/layout.tsx
 import { ReactNode } from "react";
 import { NextIntlClientProvider } from "next-intl";
 import { notFound } from "next/navigation";
 import { MainLayout } from "@/layout";
 import "../../styles/globals.css";
-import type { LayoutProps } from "next/app"; 
 
-export default async function LocaleLayout({ children, params }: LayoutProps) {
-  const locale = (params as { locale: string }).locale;
+interface LocaleLayoutProps {
+  children: ReactNode;
+  params: { locale: string };
+}
 
-  let messages: Record<string, any>;
+export default function LocaleLayout({ children, params }: LocaleLayoutProps) {
+  const { locale } = params;
+
+  let messages;
   try {
-    messages = (await import(`../../locales/${locale}.json`)).default;
+    messages = require(`../../locales/${locale}.json`);
   } catch {
     notFound();
   }
